bdc6021c6c5b3f3714ce58173c2bc63da319dd05
Tyson <79940243+theycallmetyson@users.noreply.github.com>
2024-08-21 23:59:25 +0200

    cleaning up project.

--- src/main/java/se/tcmt/taskboard/repositories/TaskListRepository.java
+++ src/main/java/se/tcmt/taskboard/repositories/TaskListRepository.java
@@ -1,9 +1,9 @@
 package se.tcmt.taskboard.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.tcmt.taskboard.entities.TaskList;
 
 @Repository
-public interface TaskListRepository extends CrudRepository<TaskList, Long> {
+public interface TaskListRepository extends JpaRepository<TaskList, Long> {
 }
